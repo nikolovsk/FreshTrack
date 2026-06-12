72f9984d9683aaf93d41b3cca167203f1a36908a
nikolovsk <nikolovskangelaa@gmail.com>
2026-06-12 19:30:00 +0200

    Rename layout directory to navbar for better clarity

--- frontend/src/layout/AppLayout.tsx
+++ frontend/src/layout/AppLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/layout/Navbar.tsx";
+import Navbar from "../components/navbar/Navbar.tsx";
 
 function AppLayout() {
     return (
